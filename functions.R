92286a340cd7eb16ab3f204b9d00b58c0a8a4b3a
Momir Milutinovic <milutinm462@gmail.com>
2020-06-10 18:08:17 +0000

    Documented functions

--- functions.R
+++ functions.R
@@ -1,9 +1,13 @@
 library(lazyeval)
 plot_treemap <- function(budget_df, city){
+  # Creates the treemap of either spending or earning
+  # @param budget_df Either prihodi or rashodi
+  # @param city The name of the city for which to plot the data
+  # @return Returns a plotly plot which contains the treemap of either earning or spending
+  
   df <- budget_df %>% filter(grad == city)
   
-  # Several grupa's can have the same ekonomska_klasa inside them so we need a group by both of these variables to make them unique
-  # We will take the sum of the groups (tibble groups) as their budgets since the functions of their entries are similar enough
+  # Creates unique labels for use in the treemap
   df <- df %>% group_by(ekonomska_klasa, grupa)
   df <- df %>% summarise(ukupno = sum(ukupno))
   df <- as.data.frame(df) %>% mutate(ekonomska_klasa = paste(ekonomska_klasa, "-", grupa))
@@ -21,7 +25,16 @@ plot_treemap <- function(budget_df, city){
 }
 
 plot_top_earning_or_spending <- function(df, title, xlabel, ylabel, color){
+  # Creates the barplot of top areas of either spending or earning
+  # @param df The budget data to plot
+  # @param title Title of the plot
+  # @param xlabel X axis label of the plot
+  # @param ylabel Y axis label of the plot
+  # @param color Color of the bars
+  # @return Returns the barplot of top areas of spending or earning depending on df parameter
+  
   df$grupa <- factor(df$grupa, levels = df$grupa[order(df$budzet)])
+  
   plot <- df %>% ggplot(aes(x=grupa, y=budzet)) +
     geom_col(fill = color, color="black") +
     theme(text = element_text(size=12)) +
@@ -34,8 +47,13 @@ plot_top_earning_or_spending <- function(df, title, xlabel, ylabel, color){
   plot
 }
 
-prepare_data_for_barplot <- function(df, city){
-  df <- df %>% filter(grad == city) %>%
+prepare_data_for_barplot <- function(budget_df, city){
+  # Gets the top 5 areas of either earning or spending for use in a barplot
+  # @param budget_df Either prihodi or rashodi
+  # @param city The name of the city for which to plot the data
+  # @return Returns a tibble with the top areas of either earning or spending
+  
+  df <- budget_df %>% filter(grad == city) %>%
     group_by(grupa)
   df <- summarise(df, budzet = sum(ukupno))
   df <- df %>% arrange(desc(budzet))
